ab6728c3e2cb632182a703b22c6ee9d605f8898c
akrush <akrush24@gmail.com>
2020-01-26 23:46:23 +0300

    Sun 26 Jan 2020 11:46:23 PM MSK

--- index.py
+++ index.py
@@ -2,21 +2,25 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def root():
     return "[HOME]"
-
-if __name__ == "__main__":
-    app.run()
 
 @app.route("/hello")
 def hello():
     return "Hello World"
 
-@app.route('/user/<username>')
+@app.route('/user/<username>', methods=['GET','POST'])
 def show_user_profile(username):
     # show the user profile for that user
     return 'User %s' % username
 
 @app.route('/about')
 def about():
-        return 'The about page'
+    return 'The about page'
+
+@app.route('/login')
+def login():
+    pass
+
+if __name__ == "__main__":
+    app.run()
