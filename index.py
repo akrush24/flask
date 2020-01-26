be017b572e6acc9e358da392612eb5d1c8facb7e
akrush <akrush24@gmail.com>
2020-01-26 23:34:57 +0300

    test

--- index.py
+++ index.py
@@ -3,11 +3,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "index!"
+    return "[HOME]"
 
 if __name__ == "__main__":
     app.run()
 
-@app.route('/hello')
+@app.route("/hello")
 def hello():
-    return 'Hello World'
+    return "Hello World"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/about')
+def about():
+        return 'The about page'
